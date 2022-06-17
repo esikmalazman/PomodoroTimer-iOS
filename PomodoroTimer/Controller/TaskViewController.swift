c57459098752c327809a6111ed15e387881aed90
Ikmal Azman <opikmalazman@gmail.com>
2022-06-17 08:28:04 +0800

    reindent vc

--- PomodoroTimer/Controller/TaskViewController.swift
+++ PomodoroTimer/Controller/TaskViewController.swift
@@ -7,35 +7,24 @@
 
 import UIKit
 
-class TaskViewController: UIViewController
-{
-
+class TaskViewController: UIViewController {
+    
     var timerBrain =  TimerBrain()
     @IBOutlet weak var workTextField: UITextField!
     @IBOutlet weak var nextBtn: UIButton!
     
-//    var workLabel : String?
+    //    var workLabel : String?
     
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         // make button round
         nextBtn.layer.cornerRadius = 25.0
         workTextField.delegate = self
     }
     
-    @IBAction func nextBtnPressed(_ sender: UIButton)
-    {
-        //notify view controller, it set as delegate
-        
-        print("I pressed")
-        
-        //set work label text
-//        workLabel = workTextField.text
+    @IBAction func nextBtnPressed(_ sender: UIButton) {
         
         //assign to timer brain function
-        if let workLabel = workTextField.text
-        {
+        if let workLabel = workTextField.text {
             timerBrain.getWorkLabel(workLabel)
         }
         
@@ -43,61 +32,46 @@ class TaskViewController: UIViewController
         // transition to another screen
         
         //if textfield null , it will not move to other screen
-        if workTextField.text != ""
-        {
+        if workTextField.text != "" {
             performSegue(withIdentifier: "goToTimer", sender: self)
-        }
-        else
-        {
+        } else{
             workTextField.placeholder = "Task To Do?"
         }
         
     }
     
     //make prepartion before transit to another screen
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //safety check for identifier
-        if segue.identifier == "goToTimer"
-        {
+        if segue.identifier == "goToTimer" {
             // set destination page to transit
             let destinationVC = segue.destination as! TimerViewController
             destinationVC.taskName = timerBrain.setLabel()
         }
     }
-    
-
-    
-
 }
 
-//MARK:- UITextFieldDelegate
-extension TaskViewController : UITextFieldDelegate
-{
+//MARK: - UITextFieldDelegate
+extension TaskViewController : UITextFieldDelegate {
     //return value to textfield if return button pressed
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool
-    {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //assign task to variable after user press next
-        if let workLabel = textField.text
-        {
+        if let workLabel = textField.text, !workLabel.isEmpty {
             timerBrain.getWorkLabel(workLabel)
-        }
-        workTextField.endEditing(true)
-        //moved to other screen after next button pressed
-        performSegue(withIdentifier: "goToTimer", sender: nextBtn)
-        return true
-    }
-    //perform validation , prevent null value
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool
-    {
-        //if textfield null, keyboard not dismiss
-        if textField.text != ""
-        {
-            
+            workTextField.resignFirstResponder()
+            performSegue(withIdentifier: "goToTimer", sender: nextBtn)
             return true
         }
-        else
-        {
+
+        return false
+    }
+    //perform validation , prevent null value
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        //if textfield null, keyboard not dismiss
+        if textField.text != "" {
+            
+            return true
+        } else {
             textField.placeholder = "Task To Do?"
             return false
         }
